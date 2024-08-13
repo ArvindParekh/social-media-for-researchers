fc7d3ac6f8fd953164e2ff790daf54050e3f9edb
Arvind <aruparekh@gmail.com>
2024-08-14 00:01:21 +0530

    chore: add format_number function to handle reactions number formatting

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -12,7 +12,20 @@ export default function PostRoot() {
    );
 }
 
+function format_number(num: number) {
+   if (num % 1000 == 0) {
+      return `${num / 1000}k`;
+   } else {
+      return num;
+   }
+}
+
 export function Post({ post }: PostProps) {
+   
+   const likes = format_number(post.reactions.likes)   
+   const comments = format_number(post.reactions.comments)   
+   const reposts = format_number(post.reactions.reposts)   
+
    return (
       <>
          <div className='card bg-base-200 flex flex-col p-2 m-2 border shadow-sm'>
@@ -33,19 +46,28 @@ export function Post({ post }: PostProps) {
             <div className='flex justify-between text-base-content'>
                <div className='flex stack'>
                   <p>
-                     <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" className="w-4 rounded-full" />
+                     <img
+                        src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+                        className='w-4 rounded-full'
+                     />
                   </p>
                   <p>
-                     <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" className="w-4 rounded-full" />
+                     <img
+                        src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+                        className='w-4 rounded-full'
+                     />
                   </p>
                   <p>
-                     <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" className="w-4 rounded-full" />
+                     <img
+                        src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+                        className='w-4 rounded-full'
+                     />
                   </p>
                </div>
                <div className='flex gap-5 text-xs text-gray-500'>
-                  <p>{post.reactions.likes} likes</p>
-                  <p>{post.reactions.comments} comments</p>
-                  <p>{post.reactions.reposts} reposts</p>
+                  <p>{likes} likes</p>
+                  <p>{comments} comments</p>
+                  <p>{reposts} reposts</p>
                </div>
             </div>
          </div>
